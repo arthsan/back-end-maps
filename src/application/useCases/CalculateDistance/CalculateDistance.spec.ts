c35bd462b7a3e2bc2dba34b4a42dc5514f87d0ba
arthsan <arthur_sbd@hotmail.com>
2022-07-19 02:07:03 -0300

    testing distance

--- src/application/useCases/CalculateDistance/CalculateDistance.spec.ts
+++ src/application/useCases/CalculateDistance/CalculateDistance.spec.ts
@@ -11,4 +11,15 @@ describe("Calculate distance useCase", () => {
 
     expect(response).toBeTruthy();
   });
+
+  it("should return the real distance = 1.19 km", async () => {
+    const coord = new CalculateDistance();
+
+    const response = await coord.execute(
+      {addressName:"Avenida Rio Branc, 1 Centro, Rio de Janeiro RJ",latitude: -22.8973551, longitude: -43.1802782},
+      {addressName:"Praça Mal. âncora, 122 Centro, Rio de Janeiro RJ", latitude: -22.9039608, longitude: -43.1703536},
+    );
+
+    expect(Number(response.toFixed(2))).toBe(1.19);
+  });
 });
